6f71b62397f8ad95e850c9fc80bb4da094668154
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-06-24 17:13:11 -0400

    test to see if deployment issue works

--- develop/src/index.js
+++ develop/src/index.js
@@ -21,12 +21,10 @@ import * as serviceWorker from './serviceWorker';
 class Root extends Component{
     render(){
         return(
-            <BrowserRouter basename={'/'}>
+            <BrowserRouter basename={'/'} component={DesignerPortfolio}>
                 <PageScrollTop>
                     <Switch>
                         <Route exact path={`/`} component={DesignerPortfolio}/>
-
-
                         <Route path={`${process.env.PUBLIC_URL}/404`} component={error404}/>
                         <Route component={error404}/>
 
