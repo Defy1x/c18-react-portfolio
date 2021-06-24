bbe27df31719004b86fcd9efde700d622032c6d4
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-06-24 15:06:30 -0400

    minor changes to contact

--- develop/src/elements/contact/ContactOne.jsx
+++ develop/src/elements/contact/ContactOne.jsx
@@ -9,9 +9,9 @@ class ContactOne extends Component{
                         <div className="col-lg-6 order-2 order-lg-1">
                             <div className="section-title text-left mb--50">
                                 <h2 className="title">Contact Me.</h2>
-                                <p className="description">I am currently looking for fullstack development work and freelance opportunities at this time.
-                                <p> Connect with me via email.</p>
-                                    <a href="mailto:cas@casmcdonald.com"> cas@casmcdonald.com</a> </p>
+                                <p className="description">I am currently looking for fullstack development work and freelance opportunities at this time.</p>
+                                <p> Connect with me via my email below.</p>
+                                    <p><a href="mailto:cas@casmcdonald.com"> cas@casmcdonald.com</a></p>
                                     <a href="https://www.casctrl.com/assets/Casandra-McDonald-Resume.pdf"><button className="rn-button-style--2 btn-solid" name="submit" id="mc-embedded-subscribe">Download Resume</button></a>
                             </div>
                         </div>
