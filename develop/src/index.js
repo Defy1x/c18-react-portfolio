ac1f241c38ba30a0f58b42dd8227736a697be5d9
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-06-24 16:54:59 -0400

    removed unused files

--- develop/src/index.js
+++ develop/src/index.js
@@ -11,29 +11,8 @@ import PageScrollTop from './component/PageScrollTop';
 import DesignerPortfolio from './home/DesignerPortfolio';
 
 // Element Layout
-import Service from "./elements/Service";
-import ServiceDetails from "./elements/ServiceDetails";
-import About from "./elements/About";
-import Contact from "./elements/Contact";
-import PortfolioDetails from "./elements/PortfolioDetails";
-import Blog from "./elements/Blog";
-import BlogDetails from "./elements/BlogDetails";
 import error404 from "./elements/error404";
 
-
-// Blocks Layout
-import Team from "./blocks/Team";
-import Counters from "./blocks/Counters";
-import Testimonial from "./blocks/Testimonial";
-import Portfolio from "./blocks/Portfolio";
-import VideoPopup from "./blocks/VideoPopup";
-import Gallery from "./blocks/Gallery";
-import Brand from "./blocks/Brand";
-import ProgressBar from "./blocks/ProgressBar";
-import ContactForm from "./blocks/ContactForm";
-import GoogleMap from "./blocks/GoogleMap";
-import Columns from "./blocks/Columns";
-import PricingTable from "./blocks/PricingTable";
 
 import { BrowserRouter, Switch, Route  } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
@@ -45,7 +24,7 @@ class Root extends Component{
             <BrowserRouter basename={'/'}>
                 <PageScrollTop>
                     <Switch>
-                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={DesignerPortfolio}/>
+                        <Route exact path={`/`} component={DesignerPortfolio}/>
 
 
                         <Route path={`${process.env.PUBLIC_URL}/404`} component={error404}/>
