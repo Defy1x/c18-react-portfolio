6cf5e29e6b48f38a6dfe4c6badf0f78f4c83eba5
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-06-24 18:25:37 -0400

    route change

--- develop/src/index.js
+++ develop/src/index.js
@@ -13,7 +13,6 @@ import DesignerPortfolio from './home/DesignerPortfolio';
 // Element Layout
 import error404 from "./elements/error404";
 
-
 import { BrowserRouter, Switch, Route  } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
@@ -21,13 +20,11 @@ import * as serviceWorker from './serviceWorker';
 class Root extends Component{
     render(){
         return(
-            <BrowserRouter basename={'/'} component={DesignerPortfolio}>
+            <BrowserRouter basename={'/'}>
                 <PageScrollTop>
                     <Switch>
                         <Route exact path={`/`} component={DesignerPortfolio}/>
-                        <Route path={`${process.env.PUBLIC_URL}/404`} component={error404}/>
                         <Route component={error404}/>
-
                     </Switch>
                 </PageScrollTop>
             </BrowserRouter>
