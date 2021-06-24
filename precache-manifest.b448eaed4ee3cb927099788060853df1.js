a826b3f7918030ccfd09e8a1a87af76dcb32329e
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-06-24 16:26:26 -0400

    Updates

---
+++ precache-manifest.b448eaed4ee3cb927099788060853df1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972f127d4bee72d5e5ce940c328e1002",
+    "url": "/Defy1x/c18-react-portfolio/index.html"
+  },
+  {
+    "revision": "e7a58e1a4ab42b14fd65",
+    "url": "/Defy1x/c18-react-portfolio/static/css/2.821c5862.chunk.css"
+  },
+  {
+    "revision": "61dffd7cd302a5841743",
+    "url": "/Defy1x/c18-react-portfolio/static/css/main.6a380a1b.chunk.css"
+  },
+  {
+    "revision": "e7a58e1a4ab42b14fd65",
+    "url": "/Defy1x/c18-react-portfolio/static/js/2.337b4774.chunk.js"
+  },
+  {
+    "revision": "61dffd7cd302a5841743",
+    "url": "/Defy1x/c18-react-portfolio/static/js/main.ba5c6704.chunk.js"
+  },
+  {
+    "revision": "efc104c9e1bbc2c02d39",
+    "url": "/Defy1x/c18-react-portfolio/static/js/runtime~main.dde30434.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Defy1x/c18-react-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Defy1x/c18-react-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Defy1x/c18-react-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Defy1x/c18-react-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
